59f2043b04e00db369e0e02f3914abca6306dfe3
Caitlin <caitlinramsey@outlook.com>
2023-05-18 21:40:05 -0400

    worked on index and lib

---
+++ index.js
@@ -0,0 +1,62 @@
+// import
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+const generateSVG = require('./lib/SVG');
+
+// questions for user input that generates logo
+
+const logoQuestions = [
+        {
+            type: 'input',
+            name: 'characters',
+            message: 'How many characters do you want for your logo?',
+            validate function (value) {
+                if(value.length > 3) {
+                    return 'Please enter three characters or less.';
+                } 
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color do you want your text to be? (keyword or hexadecimal number)',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            choices: ['Circle', 'Triangle', 'Square'],
+            message: 'What shape do you want your logo to be?',
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color do you want your shape to be? (keyword or hexadecimal number)',
+        },
+    ];
+
+inquirer.createPromptModule(logoQuestions)
+    .then(({ characters, textColor, shape, shapeColor }) => {
+        let shape;
+        switch (shape) {
+            case 'Circle':
+                shape = new Circle();
+                break;
+            case 'Triangle':
+                shape = new Triangle();
+                break;
+            case 'Square':
+                shape = new Square();
+                break;
+        }
+
+        shape.setColor(shapeColor)
+        const svg = new SVG()
+        svg.setText(text, textColor)
+        svg.setShape(shape)
+        return writeFile('./examples/logo.svg', svg.render)
+    })
+        .then (() => console.log('Generated logo.svg'))
+        .catch((err) => console.error(err));
+
+
